fec047c62d26473c8828aa05a0e97ee462b4d115
Jayshree Mishra <j1shree.in@gmail.com>
2024-10-22 17:22:00 +0530

    Data Transformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -44,6 +44,20 @@ class DataIngestion:
             raise CustomException(e, sys)
         
 #this is to test the data ingestion
+'''
+if __name__=="__main__":
+    #obj= DataIngestion()
+    #obj.initiate_data_ingestion()
+'''
+
+#this is to test data ingestion
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 if __name__=="__main__":
     obj= DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data= obj.initiate_data_ingestion()
+
+    data_transformation= DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
