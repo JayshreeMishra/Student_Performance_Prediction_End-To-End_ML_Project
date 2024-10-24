013c8b9c3ba3d9145f6e3d92a779f4303d64011c
Jayshree Mishra <j1shree.in@gmail.com>
2024-10-24 09:17:04 +0530

    Prediction pipeline added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -66,7 +66,7 @@ if __name__=="__main__":
 '''
 
 #This is to test model trainer
-
+'''
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 from src.components.model_trainer import ModelTrainer
@@ -81,3 +81,4 @@ if __name__ == "__main__":
 
     model_trainer= ModelTrainer()
     print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+'''
